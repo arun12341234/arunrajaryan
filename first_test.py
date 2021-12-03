255475243707154ffbd5c9ff4327a84307bf9d1f
arun kumar <ak080495@gmail.com>
2021-12-03 23:48:23 +0530

    make it better

---
+++ first_test.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, flash
+app = Flask(__name__)
+
+@app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+def hello_world():
+    error = 'Invalid username/password'
+    return render_template('arunrajaryan.html', error=error)
+
+
+
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run()
